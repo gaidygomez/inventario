00ed1666bab0438e63a07a7614718c81677be042
Gaidy Gomez <gggomez20@gmail.com>
2021-07-05 16:55:17 -0400

    Obtener todos los datos de todos los usuarios

--- src/pdf/reporteVentas.php
+++ src/pdf/reporteVentas.php
@@ -2,38 +2,57 @@
 require_once "../../conexion.php";
 
 $datos = [];
-$start_date = $_GET['startDate'] == '' ? date('Y-m-d') : new DateTime($_GET['startDate']);
-$end_date = $_GET['endDate'] == '' ? date('Y-m-d') : new DateTime($_GET['endDate']);
+$start_date = $_GET['startDate'] == '' ? new DateTime() : new DateTime($_GET['startDate']);
+$end_date = $_GET['endDate'] == '' ? new DateTime() : new DateTime($_GET['endDate']);
 
 if ($_GET['user'] == '' && $_GET['startDate'] == '' && $_GET['endDate'] == '') {
-	$query = "SELECT v.* FROM ventas AS v INNER JOIN usuario AS u ON v.id_usuario = u.idusuario 
-		WHERE DATE(fecha) BETWEEN $start_date AND $end_date";
-	
-	$facturas = mysqli_fetch_all(mysqli_query($conexion, $query));
+	$start = $start_date->format('Y-m-d');
+	$end = $end_date->format('Y-m-d');
 
-	foreach ($facturas as $key => $factura) {
-		$datos[] = [
-			'factura' => $factura[0],
-			'total' => $factura[2],
-			'fecha' => $factura[4]
-		];
-	}
+	$datos = allData($conexion, $start, $end);
 
-	echo json_encode(['success' => [ 'facturas' => $datos ]]);
-
-	/*$queryTotal = "SELECT SUM(v.total) FROM ventas AS v INNER JOIN usuario AS u ON v.id_usuario = u.idusuario 
-		WHERE DATE(fecha) BETWEEN $start AND $end GROUP BY v.id_usuario";
-
-
-
-	$total = mysqli_fetch_all(mysqli_query($conexion, $queryTotal));
-
-	echo json_encode(['success' => [ 'facturas' => $facturas, 'total' => $total ]]);
-
-	mysqli_close($conexion);*/
-
+	echo $datos;
 } elseif ($_GET['user'] != '' && $_GET['startDate'] == '' && $_GET['endDate'] == '') {
 	echo json_encode('Datos de hoy del usuario específico');
 } else {
 	echo json_encode('Datos Especificos');
+}
+
+function allData($conexion, $start, $end) {
+	$data = [];
+
+	$query = "SELECT v.*, u.nombre as usuario, u.correo, c.nom, c.nombre as cliente, c.telefono 
+	FROM inventario.ventas AS v 
+		INNER JOIN inventario.usuario AS u 
+			ON v.id_usuario = u.idusuario
+		INNER JOIN inventario.cliente as c
+			ON v.id_cliente = c.idcliente 
+	WHERE DATE(fecha) BETWEEN ? AND ?";
+	
+	$stmt = mysqli_prepare($conexion ,$query);
+
+	mysqli_stmt_bind_param($stmt, 'ss', $start, $end);
+
+	mysqli_stmt_execute($stmt);
+
+	$datos = mysqli_stmt_get_result($stmt);
+
+	$facturas = mysqli_fetch_all($datos);
+
+	mysqli_close($conexion);
+
+	foreach ($facturas as $key => $factura) {
+		$data[] = [
+			'factura' => $factura[0],
+			'total' => $factura[2],
+			'fecha' => $factura[4],
+			'usuario' => $factura[5],
+			'usuario_correo' => $factura[6],
+			'cliente_nombre' => $factura[7],
+			'cliente_dni' => $factura[8],
+			'cliente_telefono' => $factura[9] 
+		];
+	}
+
+	return json_encode(['success' => [ 'facturas' => $data ]]);
 }
