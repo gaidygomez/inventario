f3891fe143e6f0d7206958077beb0653d9619666
Gaidy Gomez <gggomez20@gmail.com>
2021-07-15 07:22:34 -0400

    corrección en actualización de campos de ventas

--- src/updateTemp.php
+++ src/updateTemp.php
@@ -5,7 +5,7 @@ session_start();
 
 $user = $_SESSION['idUser'];
 
-if (isset($_POST['id']) && isset($_POST['qty'])) {
+if (isset($_POST['id']) && $_POST['qty'] != '') {
     $qty = "UPDATE detalle_temp SET cantidad = ? WHERE id = ?";
 
     $stmt = mysqli_prepare($conexion, $qty);
@@ -14,7 +14,7 @@ if (isset($_POST['id']) && isset($_POST['qty'])) {
 
     echo json_encode(mysqli_stmt_execute($stmt));
 
-} elseif (isset($_POST['id']) && isset($_POST['price'])) {
+} elseif (isset($_POST['id']) && $_POST['price'] != '') {
     $price = "UPDATE detalle_temp SET precio_venta = ?, total = precio_venta * cantidad WHERE id = ?";
 
     $stmt = mysqli_prepare($conexion, $price);
